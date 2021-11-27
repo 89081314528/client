b4b023429899683f2fcd6cbea08dc5fc6aef7a11
julia <89081314528@mail.ru>
2021-11-27 22:16:33 +0300

    27/11

--- src/main/java/ru/julia/client/clients/FileStorageClient.java
+++ src/main/java/ru/julia/client/clients/FileStorageClient.java
@@ -7,21 +7,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+import ru.julia.client.dto.FilesToDeleteFromServerAndClient;
 import ru.julia.client.dto.FilesToTransferAndReceive;
 
 import java.util.List;
+
 // аннотация обозначает, что реализацию интерфейса спринг сделает сам и
 @FeignClient(name = "localhost", url = "http://localhost:8080/", configuration = FeignSupportConfig.class)
 public interface FileStorageClient {
     @RequestMapping("/getFiles")
     FilesToTransferAndReceive filesToTransferAndReceive(@RequestParam("filesFromClient") List<String> filesFromClient,
-                                                               @RequestParam("clientName")String clientName);
+                                                        @RequestParam("clientName") String clientName);
 
-    @RequestMapping("/transferToClient") // строит запрос для сервера
-    String transferToClient(@RequestParam("clientName") String clientName, @RequestParam("fileName") String fileName);
+    @RequestMapping("/transferToClient")
+        // строит запрос для сервера
+    String transferToClient(@RequestParam("clientName") String clientName,
+                            @RequestParam("fileName") String fileName);
 
     @PostMapping(value = "/receiveFromClient", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    String receiveFromClient(@RequestParam("clientName") String clientName, @RequestParam("fileName") String fileName,
-                   @RequestPart(value = "file") MultipartFile file);
+    String receiveFromClient(@RequestParam("clientName") String clientName,
+                             @RequestParam("fileName") String fileName,
+                             @RequestPart(value = "file") MultipartFile file);
+
+    @RequestMapping("/deleteFiles")
+    FilesToDeleteFromServerAndClient filesToDeleteFromServerAndClient(
+            @RequestParam("filesFromClient") List<String> filesFromClient,
+            @RequestParam("clientName") String clientName);
+
+    @RequestMapping("/deleteFromServer")
+    String deleteFromServer(@RequestParam("clientName") String clientName,
+                            @RequestParam("fileName") String fileName);
 
 }
--- src/main/java/ru/julia/client/servicies/ClientServiceImpl.java
+++ src/main/java/ru/julia/client/servicies/ClientServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import ru.julia.client.clients.FileStorageClient;
+import ru.julia.client.dto.FilesToDeleteFromServerAndClient;
 import ru.julia.client.dto.FilesToTransferAndReceive;
 
 import java.io.File;
@@ -22,31 +23,38 @@ public class ClientServiceImpl implements ClientService {
 
     @Override
     public void answerFromServer() {
-        List<String> clientFiles = new ArrayList<>();
-        String dirPath = "C:/Users/julia/Desktop/client";
-        File dir = new File(dirPath);
-        File[] fileList = dir.listFiles();
-        if (fileList != null) {
-            for (File file : fileList) {
-                clientFiles.add(file.getName());
-            }
-        } else {
-            throw new RuntimeException("Client storage directory not found");
-        }
+        List<String> clientFiles = makeClientFilesList();
         String clientName = "julia";
+
+        FilesToDeleteFromServerAndClient filesToDeleteFromServerAndClient =
+                fileStorageClient.filesToDeleteFromServerAndClient(clientFiles, clientName);
+        // удаление с сервера
+//        List<String> filesToDeleteFromServer = filesToDeleteFromServerAndClient.getFilesToDeleteFromServer();
+//        for (String s : filesToDeleteFromServer) {
+//            System.out.println("удален файл на сервере " + s);
+//            fileStorageClient.deleteFromServer(clientName, s);
+//        }
+        // удаление с клиента
+//        List<String> filesToDeleteFromClient = filesToDeleteFromServerAndClient.getFilesToDeleteFromClient();
+//        for (String s : filesToDeleteFromClient) {
+//            System.out.println("удален файл у клиента " + s);
+//            File file = new File("C:/Users/julia/Desktop/" + clientName + "/" + s);
+//            file.delete();
+//        }
+
         FilesToTransferAndReceive filesToTransferAndReceive =
                 fileStorageClient.filesToTransferAndReceive(clientFiles, clientName);
-
-        List<String> filesToReceiveFromClient = filesToTransferAndReceive.getFilesToReceive();
+        // добавление клиенту с сервера
 //        List<String> filesToTransferToClient = filesToTransferAndReceive.getFilesToTransfer();
-
 //        for (String s : filesToTransfer) {
 //            fileStorageClient.transfer(clientName, s);
 //            System.out.println("Передан файл " + s);
 //        }
+        // добавление на сервер с клиента
+        List<String> filesToReceiveFromClient = filesToTransferAndReceive.getFilesToReceive();
         for (String s : filesToReceiveFromClient) {
             System.out.println("Получен файл " + s);
-            String path = "C:/Users/julia/Desktop/client/" + s;
+            String path = "C:/Users/julia/Desktop/" + clientName + "/" + s;
             File file = new File(path);
             DiskFileItem fileItem = new DiskFileItem("file", "application/octet-stream",
                     false, file.getName(), (int) file.length(), file.getParentFile());
@@ -56,9 +64,23 @@ public class ClientServiceImpl implements ClientService {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            //        fileItem.getOutputStream();
             MultipartFile multipartFile = new CommonsMultipartFile(fileItem);
             fileStorageClient.receiveFromClient(clientName, s, multipartFile);
         }
+    }
+
+    List<String> makeClientFilesList() {
+        List<String> clientFiles = new ArrayList<>();
+        String dirPath = "C:/Users/julia/Desktop/julia";
+        File dir = new File(dirPath);
+        File[] fileList = dir.listFiles();
+        if (fileList != null) {
+            for (File file : fileList) {
+                clientFiles.add(file.getName());
+            }
+        } else {
+            throw new RuntimeException("Client storage directory not found");
+        }
+        return clientFiles;
     }
 }
